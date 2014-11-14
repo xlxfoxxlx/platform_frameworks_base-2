988c035b3726dd31d335f9b92c47401c47eb8b63
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2014-11-14 09:44:49 -0800

    Dialer lookup
    Squashed commit of the following:
    
    commit 780211109f80d07b11d7a0261544b1a0fed10ea0
    Author: Xiao-Long Chen <chenxiaolong@cxl.epac.to>
    Date:   Fri Mar 7 17:11:02 2014 -0500
    
        Add people lookup settings keys.
    
        Change-Id: I2ff9bb7684d04f4f9b640939c7db269873aa3308
    
    commit 0e5ed5ac782c2ee253179a03a2c5842ad3de6831
    Author: Xiao-Long Chen <chenxiaolong@cxl.epac.to>
    Date:   Fri Jan 31 11:43:10 2014 -0500
    
        Frameworks: Add settings keys for forward and reverse phone number
        lookup
    
        Change-Id: I1c4c7825b4361b7a52dd419f83afd7b9fcbe1494
    
    commit 3cd778a712234332f327d7e63050c216f21515ac
    Author: Xiao-Long Chen <chenxiaolong@cxl.epac.to>
    Date:   Sat Apr 12 15:26:29 2014 -0400
    
        Add OpenCnam paid account settings keys
    
        Change-Id: I895b5f2b2a8a8de568e5aa1cea5d66ba490985fc
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3580,6 +3580,54 @@ public final class Settings {
         public static final String STATUS_BAR_BATTERY_STATUS_ICON_INDICATOR = "status_bar_battery_status_icon_indicator";
 
         /**
+         * The OpenCNAM paid account ID
+         * @hide
+         */
+        public static final String DIALER_OPENCNAM_ACCOUNT_SID = "dialer_opencnam_account_sid";
+
+        /**
+         * The OpenCNAM authentication token
+         * @hide
+         */
+        public static final String DIALER_OPENCNAM_AUTH_TOKEN = "dialer_opencnam_auth_token";
+
+        /**
+         * Enable looking up of phone numbers of nearby places
+         * @hide
+         */
+        public static final String ENABLE_FORWARD_LOOKUP = "enable_forward_lookup";
+
+        /**
+         * Enable looking up of phone numbers of people
+         * @hide
+         */
+        public static final String ENABLE_PEOPLE_LOOKUP = "enable_people_lookup";
+
+        /**
+         * Enable looking up of information of phone numbers not in the contacts
+         * @hide
+         */
+        public static final String ENABLE_REVERSE_LOOKUP = "enable_reverse_lookup";
+
+        /**
+         * The forward lookup provider
+         * @hide
+         */
+        public static final String FORWARD_LOOKUP_PROVIDER = "forward_lookup_provider";
+
+        /**
+         * The people lookup provider
+         * @hide
+         */
+        public static final String PEOPLE_LOOKUP_PROVIDER = "people_lookup_provider";
+
+        /**
+         * The reverse lookup provider
+         * @hide
+         */
+        public static final String REVERSE_LOOKUP_PROVIDER = "reverse_lookup_provider";
+
+        /**
          *  Weather to show the battery status text on the status bar
          * @hide
          */
@@ -3691,6 +3739,7 @@ public final class Settings {
          * @hide
          */
         public static final String INCREASING_RING_START_VOLUME = "increasing_ring_start_vol";
+
 
         /**
          * Ramp up time (seconds) for increasing ring
