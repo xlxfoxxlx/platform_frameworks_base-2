5d36583f110d18be2b1960f0140c52c91d8b2779
Evisceration <eviscerationls@gmail.com>
2015-01-01 14:29:50 +0000

    [1/2] Quick settings: Add Haptic Feedback to tiles
    Change-Id: I792a00a2689a572110963db00362f8e9d77b316e
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/qs/QSPanel.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3688,6 +3688,12 @@ public final class Settings {
          */
         public static final String QS_SHOW_BRIGHTNESS_SLIDER = "qs_show_brightness_slider";
 
+        /** Whether to vibrate when quick settings tile is pressed.
+          *
+          * @hide
+          */
+        public static final String QUICK_SETTINGS_TILES_VIBRATE = "quick_settings_vibrate";
+
         /**
          * Whether to use the custom status bar header or not
          * @hide
