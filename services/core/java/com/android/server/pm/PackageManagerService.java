b529719e276959378b6e4a7bbe2439dfc96219c6
dhacker29 <dhackerdvm@gmail.com>
2015-11-24 01:53:47 -0500

    Core: Use ro.build.date to signal mIsUpgrade
    Modified the original commit by dhacker29 to check for build date
    
    Original commit by dhacker29:
    Core: Use ro.purenexus.version to signal mIsUpgrade
    We use a static fingerprint that is only changed when a new stock
    version is released, so every flash shows Android is starting instead
    of upgrading.  This will fix that.
    
    Change-Id: Ic5559b662f5e6fc4df1f6929494663bcb84f8333

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -2053,7 +2053,7 @@ public class PackageManagerService extends IPackageManager.Stub {
             }
 
             final VersionInfo ver = mSettings.getInternalVersion();
-            mIsUpgrade = !Build.FINGERPRINT.equals(ver.fingerprint);
+            mIsUpgrade = !Build.DATE.equals(ver.fingerprint);
             // when upgrading from pre-M, promote system app permissions from install to runtime
             mPromoteSystemApps =
                     mIsUpgrade && ver.sdkVersion <= Build.VERSION_CODES.LOLLIPOP_MR1;
@@ -2352,7 +2352,7 @@ public class PackageManagerService extends IPackageManager.Stub {
                         deleteCodeCacheDirsLI(ps.volumeUuid, ps.name);
                     }
                 }
-                ver.fingerprint = Build.FINGERPRINT;
+                ver.fingerprint = Build.DATE;
             }
 
             checkDefaultBrowser();
@@ -15774,7 +15774,7 @@ public class PackageManagerService extends IPackageManager.Stub {
                     Slog.w(TAG, "Failed to scan " + ps.codePath + ": " + e.getMessage());
                 }
 
-                if (!Build.FINGERPRINT.equals(ver.fingerprint)) {
+                if (!Build.DATE.equals(ver.fingerprint)) {
                     deleteCodeCacheDirsLI(ps.volumeUuid, ps.name);
                 }
             }
