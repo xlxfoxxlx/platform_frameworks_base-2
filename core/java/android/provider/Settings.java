d1a2593e5e4dba797c89b81bcd09879636d02f76
Danny Baumann <dannybaumann@web.de>
2015-12-27 17:06:44 +0100

    [1/3] Add increasing ring feature
    Change-Id: Icf46f97a607fb791f6ec45066940d5d0859467bc

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3871,6 +3871,24 @@ public final class Settings {
         public static final String DOZE_PULSE_DURATION_OUT = "doze_pulse_duration_out";
 
         /**
+         * Whether the phone ringtone should be played in an increasing manner
+         * @hide
+         */
+        public static final String INCREASING_RING = "increasing_ring";
+
+        /**
+         * Start volume fraction for increasing ring volume
+         * @hide
+         */
+        public static final String INCREASING_RING_START_VOLUME = "increasing_ring_start_vol";
+
+        /**
+         * Ramp up time (seconds) for increasing ring
+         * @hide
+         */
+        public static final String INCREASING_RING_RAMP_UP_TIME = "increasing_ring_ramp_up_time";
+
+        /**
          * Duration visible for ambient display
          * @hide
          */
@@ -4558,8 +4576,10 @@ public final class Settings {
             ACCELEROMETER_ROTATION,
             NOTIFICATION_LIGHT_SCREEN_ON,
             ALLOW_LIGHTS,
-            NOTIFICATION_PLAY_QUEUE
-
+            NOTIFICATION_PLAY_QUEUE,
+            INCREASING_RING,
+            INCREASING_RING_START_VOLUME,
+            INCREASING_RING_RAMP_UP_TIME
         };
 
         /**
