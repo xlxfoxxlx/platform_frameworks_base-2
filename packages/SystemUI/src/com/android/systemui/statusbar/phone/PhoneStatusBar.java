b0e2919a951c396d60640a92789941175347e85d
Randall Rushing <randall.rushing@gmail.com>
2016-01-27 22:53:52 -0800

    Smartbar: Cripple screen pinning confirmation and get right to it
    Due to our new task lock hint api an instructional dialog is
    no longer needed or desired. Also, it is rather impractical to
    keep the dialog but create some kind of "on the fly" dynamic
    layout in the dialog. So when the screen pinning command comes
    down the pipes, just fire it.
    
    Change-Id: Iaed77e9965aa18509fad9c881220ab836897c3e3

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -4195,7 +4195,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     }
 
     public void showScreenPinningRequest(boolean allowCancel) {
-        mScreenPinningRequest.showPrompt(allowCancel);
+        //mScreenPinningRequest.showPrompt(allowCancel);
+        try {
+            ActivityManagerNative.getDefault().startLockTaskModeOnCurrent();
+        } catch (RemoteException e) {}
     }
 
     public boolean hasActiveNotifications() {
