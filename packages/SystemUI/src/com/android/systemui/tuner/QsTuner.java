ResurrectionRemix/android_frameworks_base

QS : Pulse Tile
--No this is not a visualizer Tile. This a simple Tile that toggles Pulse and Lavamp Effect
SinglePress to Toggle Pulse
LongPress to Toggle Lavamp

Lavamp effect will only be triggered if Pulse is Enabled

Thanks to Alex Cruz, Randall And Other DU and Team EOS  members who have contributed to Pulse

Signed-off-by: Varun Date <date.varun123@gmail.com>

packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
@@ -236,6 +236,7 @@ public class QsTuner extends Fragment implements Callback {
         else if (mSpec.equals("hw_keys")) return R.string.quick_settings_hwkeys_title;
         else if (mSpec.equals("sound")) return R.string.quick_settings_sound_label;
         else if (mSpec.equals("lockscreen")) return R.string.quick_settings_lockscreen_label;
+        else if (mSpec.equals("pulse")) return R.string.quick_settings_pulse_label;
         return 0;
     }
 
@@ -491,6 +492,7 @@ public class QsTuner extends Fragment implements Callback {
 	    else if (mSpec.equals("hw_keys")) return R.drawable.ic_qs_hwkeys_on;
 	    else if (mSpec.equals("sound")) return R.drawable.ic_qs_ringer_silent;
 	    else if (mSpec.equals("lockscreen")) return R.drawable.ic_qs_lock_screen_on;
+	    else if (mSpec.equals("pulse")) return R.drawable.ic_qs_pulse;
             return R.drawable.android;
         }
 

xlxfoxxlx/platform_frameworks_base-2

Conflicts:
	packages/SystemUI/res/values/config.xml
	packages/SystemUI/res/values/rr_strings.xml
	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
	packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
	packages/SystemUI/res/values/custom_strings.xml

packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
@@ -224,6 +224,7 @@ public class QsTuner extends PreferenceFragment implements Callback {
         else if (spec.equals("navigation_bar")) return R.string.quick_settings_navigation_bar;
         else if (spec.equals("volume")) return R.string.quick_settings_volume_panel_label;
         else if (spec.equals("compass")) return R.string.quick_settings_compass_label;
+        else if (spec.equals("pulse")) return R.string.quick_settings_pulse_label;
         return 0;
     }
 
@@ -486,6 +487,7 @@ public class QsTuner extends PreferenceFragment implements Callback {
             else if (mSpec.equals("navigation_bar")) return R.drawable.ic_qs_smartbar;
             else if (mSpec.equals("volume")) return R.drawable.ic_qs_volume_panel;
             else if (mSpec.equals("compass")) return R.drawable.ic_qs_compass_on;
+	    else if (mSpec.equals("pulse")) return R.drawable.ic_qs_pulse;
             return R.drawable.android;
         }
 