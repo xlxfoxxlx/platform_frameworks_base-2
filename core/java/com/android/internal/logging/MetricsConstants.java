14b784c35ee8f57a99f9911b31c7842a091ab461
mydongistiny <jaysonedson@gmail.com>
2016-07-10 12:45:33 -0700

    QS: Add Substratum Tile
    Change-Id: I0783bb38db68b3889d5931aa316e0ad148b56de4

--- core/java/com/android/internal/logging/MetricsConstants.java
+++ core/java/com/android/internal/logging/MetricsConstants.java
@@ -270,6 +270,7 @@ public interface MetricsConstants {
     public static final int QS_KERNEL = 244;
     public static final int QS_CAFFEINE = 245;
     public static final int QS_NAVBAR_TOGGLE = 246;
+    public static final int QS_SUBSTRATUM = 247;
 
     // Lock screen weather settings
     public static final int LOCKSCREEN_WEATHER_SETTINGS = 248;
