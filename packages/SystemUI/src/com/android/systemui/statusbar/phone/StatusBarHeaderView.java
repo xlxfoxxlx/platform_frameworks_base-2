AICP/frameworks_base

Halo: Fix up header button icon, position and animation
Change-Id: I119c754cb24ccc43ccd0c326225b25bb48667706

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
@@ -913,11 +913,13 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
                 ? 0
                 : mMultiUserSwitch.getLeft() - mSettingsContainer.getLeft();
         target.haloButtonAlpha = getAlphaForVisibility(mHaloButton);
-        target.haloButtonTranslation = mExpanded ? 0 : mSettingsContainer.getLeft() - mHaloButton.getLeft();
+        target.haloButtonTranslation = mExpanded
+                ? 0
+                : mSettingsContainer.getLeft() - mHaloButton.getLeft();
         target.taskManagerAlpha = getAlphaForVisibility(mTaskManagerButton);
         target.taskManagerTranslation = mExpanded
                 ? 0
-                : mSettingsContainer.getLeft() - mTaskManagerButton.getLeft();
+                : mHaloButton.getLeft() - mTaskManagerButton.getLeft();
         target.signalClusterAlpha = mSignalClusterDetached ? 0f : 1f;
         target.settingsRotation = !mExpanded ? 90f : 0f;
     }
@@ -978,7 +980,9 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
         mTaskManagerButton.setTranslationY(mSystemIconsSuperContainer.getTranslationY());
         mTaskManagerButton.setTranslationX(values.taskManagerTranslation);
         mTaskManagerButton.setRotation(values.settingsRotation);
+        mHaloButton.setTranslationY(mSystemIconsSuperContainer.getTranslationY());
         mHaloButton.setTranslationX(values.haloButtonTranslation);
+        mHaloButton.setRotation(values.settingsRotation);
         applyAlpha(mEmergencyCallsOnly, values.emergencyCallsOnlyAlpha);
         if (!mShowingDetail && !mDetailTransitioning) {
             // Otherwise it needs to stay invisible
@@ -1090,7 +1094,7 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
                     v1.taskManagerTranslation * (1 - t) + v2.taskManagerTranslation * t;
             settingsTranslation = v1.settingsTranslation * (1 - t) + v2.settingsTranslation * t;
             weatherY = v1.weatherY * (1 - t) + v2.weatherY * t;
-            haloButtonTranslation = v1.haloButtonTranslation * (1 -t) + v2.haloButtonTranslation * t;
+            haloButtonTranslation = v1.haloButtonTranslation * (1 - t) + v2.haloButtonTranslation * t;
 
             float t1 = Math.max(0, t - 0.5f) * 2;
             settingsRotation = v1.settingsRotation * (1 - t1) + v2.settingsRotation * t1;
@@ -1239,7 +1243,7 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
         return Settings.Secure.putInt(mContext.getContentResolver(),
                     Settings.Secure.HALO_ACTIVE, 1);
     }
-    
+
     class SettingsObserver extends UserContentObserver {
         SettingsObserver(Handler handler) {
             super(handler);

xlxfoxxlx/platform_frameworks_base-2

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
@@ -775,7 +775,9 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
                 ? 0
                 : mMultiUserSwitch.getLeft() - mSettingsButton.getLeft();
         target.haloButtonAlpha = getAlphaForVisibility(mHaloButton);
-        target.haloButtonTranslation = mExpanded ? 0 : mSettingsButton.getLeft() - mHaloButton.getLeft();
+        target.haloButtonTranslation = mExpanded
+                ? 0
+                : mSettingsButton.getLeft() - mHaloButton.getLeft();
         target.signalClusterAlpha = mSignalClusterDetached ? 0f : 1f;
         target.settingsRotation = !mExpanded ? 90f : 0f;
         target.weatherImageAlpha =  getAlphaForVisibility(mWeatherImage);
@@ -827,11 +829,14 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
         } else if (mSignalCluster != null) {
             mSignalCluster.setTranslationX(0f);
             mSignalCluster.setTranslationY(0f);
+        
+            mSettingsButton.setTranslationY(mSystemIconsSuperContainer.getTranslationY());
+            mSettingsButton.setTranslationX(values.settingsTranslation);
+            mSettingsButton.setRotation(values.settingsRotation);
         }
-        mSettingsButton.setTranslationY(mSystemIconsSuperContainer.getTranslationY());
-        mSettingsButton.setTranslationX(values.settingsTranslation);
-        mSettingsButton.setRotation(values.settingsRotation);
+        mHaloButton.setTranslationY(mSystemIconsSuperContainer.getTranslationY());
         mHaloButton.setTranslationX(values.haloButtonTranslation);
+        mHaloButton.setRotation(values.settingsRotation);
         applyAlpha(mEmergencyCallsOnly, values.emergencyCallsOnlyAlpha);
         if (!mShowingDetail && !mDetailTransitioning) {
             // Otherwise it needs to stay invisible
@@ -889,7 +894,7 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
             batteryX = v1.batteryX * (1 - t) + v2.batteryX * t;
             batteryY = v1.batteryY * (1 - t) + v2.batteryY * t;
             settingsTranslation = v1.settingsTranslation * (1 - t) + v2.settingsTranslation * t;
-            haloButtonTranslation = v1.haloButtonTranslation * (1 -t) + v2.haloButtonTranslation * t;
+            haloButtonTranslation = v1.haloButtonTranslation * (1 - t) + v2.haloButtonTranslation * t;
 
             float t1 = Math.max(0, t - 0.5f) * 2;
             settingsRotation = v1.settingsRotation * (1 - t1) + v2.settingsRotation * t1;