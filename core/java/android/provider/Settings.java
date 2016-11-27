96a454036572d4612a1dac25509ccf61e20e9dce
Lars Greiss <kufikugel@googlemail.com>
2016-11-27 11:55:30 +0100

    Option to disable music track title in QS tile [1/2]
    Idea taken from @kufikugel's old 'qs tweeks' commit
    https://github.com/SlimRoms/frameworks_base/commit/4bc584e721c9558657deca7ac9de31b7f0ca3af3
    
    Rewritten by @Mazda--
    
    Change-Id: I34d58ad090ff01ddf3d75da52ae06c5973c1f51b

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4033,6 +4033,12 @@ public final class Settings {
         public static final String KEYGUARD_SHOW_CLOCK = "keyguard_show_clock";
 
         /**
+         * Whether to display music track title within the music qs tile
+         * @hide
+         */
+        public static final String MUSIC_TILE_TITLE = "music_tile_title";
+
+        /**
          * Disable dashboard conditions in settings
          * @hide
          */
